dade50c1350a877f376efe20dd8d3d2626b68506
Plasmoxy <sebohacker@gmail.com>
2020-02-15 12:46:18 +0100

    add ImmerHook return type, fix import of React Dispatch type

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import produce, { Draft } from "immer";
-import { useState, useReducer, useCallback } from "react";
+import { useState, useReducer, useCallback, Dispatch } from "react";
 
 export type Reducer<S = any, A = any> = (
   draftState: Draft<S>,
   action: A
 ) => void | S;
 
+export type ImmerHook<S> = [S, (f: (draft: Draft<S>) => void | S) => void]
+
 export function useImmer<S = any>(
   initialValue: S | (() => S)
 ): [S, (f: (draft: Draft<S>) => void | S) => void];
+
 export function useImmer(initialValue: any) {
   const [val, updateValue] = useState(initialValue);
   return [
@@ -23,7 +26,7 @@ export function useImmerReducer<S = any, A = any>(
   reducer: Reducer<S, A>,
   initialState: S,
   initialAction?: (initial: any) => S
-): [S, React.Dispatch<A>];
+): [S, Dispatch<A>];
 export function useImmerReducer(reducer, initialState, initialAction) {
   const cachedReducer = useCallback(produce(reducer), [reducer]);
   return useReducer(cachedReducer, initialState as any, initialAction);
