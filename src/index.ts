80e1fe2e88765ed29cbc88af153f81a42dbd8b5d
Héctor Echiburú <black.shot@live.cl>
2021-03-18 22:39:21 +0000

    Enables passing a direct value to the useCallback hook for using it as simple state

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,17 @@ export type ImmerHook<S> = [S, Updater<S>];
 
 export function useImmer<S = any>(
   initialValue: S | (() => S)
-): [S, (f: (draft: Draft<S>) => void | S) => void];
+): [S, (f: (draft: Draft<S> | S) => void | S) => void];
 
 export function useImmer(initialValue: any) {
   const [val, updateValue] = useState(initialValue);
   return [
     val,
     useCallback(updater => {
-      updateValue(produce(updater));
+      if(typeof updater === "function")
+        updateValue(produce(updater));
+      else
+        updateValue(updater);
     }, [])
   ];
 }
