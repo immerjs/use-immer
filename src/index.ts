4354b38edea270db5d3d338965224991404e372b
Omer Prizner <omer.prizner@gmail.com>
2021-04-23 20:31:54 +0300

    Improve typings, updater will now return promise of the new value instead of void

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,34 @@ import { useState, useReducer, useCallback, useMemo, Dispatch } from "react";
 export type Reducer<S = any, A = any> = (
   draftState: Draft<S>,
   action: A
-) => void | S | (S extends undefined ? typeof nothing : never);
+) => void | (S extends undefined ? typeof nothing : S);
 
-export type Updater<S> = (f: (draft: Draft<S>) => void | S) => void;
+export type Updater<S> = (f: ((draft: Draft<S>) => void) | S) => Promise<S>;
 
 export type ImmerHook<S> = [S, Updater<S>];
 
-export function useImmer<S = any>(
-  initialValue: S | (() => S)
-): [S, (f: ((draft: Draft<S> | S) => void) | S) => void];
+export function useImmer<S = any>(initialValue: S | (() => S)): ImmerHook<S>;
 
 export function useImmer(initialValue: any) {
   const [val, updateValue] = useState(initialValue);
   return [
     val,
-    useCallback(updater => {
-      if(typeof updater === "function")
-        updateValue(produce(updater));
-      else
-        updateValue(updater);
-    }, [])
+    useCallback((updater) => {
+      if (typeof updater === "function") {
+        let nextValue: any;
+        return new Promise((resolve) => {
+          updateValue((curr: any) => {
+            nextValue = produce(curr, updater);
+            resolve(nextValue);
+            return nextValue;
+          });
+        });
+      } else {
+        const nextValue = updater;
+        updateValue(nextValue);
+        return Promise.resolve(nextValue);
+      }
+    }, []),
   ];
 }
 
@@ -32,7 +40,11 @@ export function useImmerReducer<S = any, A = any>(
   initialState: S,
   initialAction?: (initial: any) => S
 ): [S, Dispatch<A>];
-export function useImmerReducer(reducer: any, initialState: any, initialAction: any) {
+export function useImmerReducer(
+  reducer: any,
+  initialState: any,
+  initialAction: any
+) {
   const cachedReducer = useMemo(() => produce(reducer), [reducer]);
   return useReducer(cachedReducer, initialState as any, initialAction);
 }
