df3ec972a14a45039205a7e9c51ff1dfc26f3406
Héctor Echiburú <black.shot@live.cl>
2021-05-15 15:52:24 -0400

    fix: Removed duplicated types and improved for readability (#78)

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,19 @@ export type Reducer<S = any, A = any> = (
   draftState: Draft<S>,
   action: A
 ) => void | S | (S extends undefined ? typeof nothing : never);
-
-export type Updater<S> = (f: (draft: Draft<S>) => void | S) => void;
-
+export type DraftFunction<S> = (draft: Draft<S>) => void;
+export type Updater<S> = (arg: S | DraftFunction<S>) => void;
 export type ImmerHook<S> = [S, Updater<S>];
-
-export function useImmer<S = any>(
-  initialValue: S | (() => S)
-): [S, (f: ((draft: Draft<S> | S) => void) | S) => void];
+export function useImmer<S = any>(initialValue: S | (() => S)): ImmerHook<S>;
 
 export function useImmer(initialValue: any) {
   const [val, updateValue] = useState(initialValue);
   return [
     val,
-    useCallback(updater => {
-      if(typeof updater === "function")
-        updateValue(produce(updater));
-      else
-        updateValue(updater);
-    }, [])
+    useCallback((updater) => {
+      if (typeof updater === "function") updateValue(produce(updater));
+      else updateValue(updater);
+    }, []),
   ];
 }
 
@@ -32,7 +26,11 @@ export function useImmerReducer<S = any, A = any>(
   initialState: S,
   initialAction?: (initial: any) => S
 ): [S, Dispatch<A>];
-export function useImmerReducer(reducer: any, initialState: any, initialAction: any) {
+export function useImmerReducer(
+  reducer: any,
+  initialState: any,
+  initialAction: any
+) {
   const cachedReducer = useMemo(() => produce(reducer), [reducer]);
   return useReducer(cachedReducer, initialState as any, initialAction);
 }
